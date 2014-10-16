87a8b99eca0ee996ef4d1fcc76870d889457ae36
David Hempy <dhempy@users.noreply.github.com>
2014-10-15 23:54:58 -0400

    error requiring 'aws-sdk'

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,9 +1,5 @@
 
-## Should these requires be somewhere else?  probably...
-require 'rubygems'
-require 'bundler/setup'
 require 'aws-sdk'
-require 'uuid'
 
 class Resource < ActiveRecord::Base
 
@@ -15,9 +11,12 @@ class Resource < ActiveRecord::Base
 		s3 = AWS::S3.new
 		bucket_name = "lib-share-cache-00254b-9fdc5a"
 		bucket = s3.buckets[bucket_name]
+		
+		# Todo: actually fetch the content of the url's object.  But first, try try to create a simple file on s3.
+
 		object = bucket.objects.create('url.txt',self.url)
 		
-		self.comments += "Cache URL: " + object.url_for(:read)	
+		self.comments += "Cached URL: " + object.url_for(:read)	
 		
 	end
 	 
